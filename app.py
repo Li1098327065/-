b0cea8438fad1e11e4092b894526f131b575a951
Li1098327065 <YLI157@e.ntu.edu.sg>
2025-02-14 09:44:14 +0000

    1

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/business')
+def business():
+    return render_template('business.html')
+
+@app.route('/service')
+def service():
+    return render_template('service.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
