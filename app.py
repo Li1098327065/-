bcde590ecf2f087aa24c078ada89f2062fb8676e
Li1098327065 <YLI157@e.ntu.edu.sg>
2025-02-20 06:10:24 +0000

    1

--- app.py
+++ app.py
@@ -18,5 +18,6 @@ def business():
 def service():
     return render_template('service.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
